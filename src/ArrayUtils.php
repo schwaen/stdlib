1fe760c36ab14daba50d5808c24ffe7ce8a28e79
plajo <s.muhle@plajo.de>
2016-04-24 18:03:30 +0200

    first function and tests plus composer.json

---
+++ src/ArrayUtils.php
@@ -0,0 +1,37 @@
+<?php
+namespace Schwaen\Stdlib;
+
+/**
+ * ArrayUtils
+ */
+class ArrayUtils {
+  /**
+   * column
+   *
+   * @access public
+   * @static
+   * @param array $input
+   * @param unknown $column_key
+   * @param string $index_key
+   * @return null|array
+   */
+  public static function column($input, $column_key, $index_key = null) {
+    $arr = array_map(function($d) use ($column_key, $index_key) {
+      if (!isset($d[$column_key])) {
+        return null;
+      }
+      if ($index_key !== null) {
+        return array($d[$index_key] => $d[$column_key]);
+      }
+      return $d[$column_key];
+    }, $input);
+    if ($index_key !== null) {
+      $tmp = array();
+      foreach ($arr as $ar) {
+        $tmp[key($ar)] = current($ar);
+      }
+      $arr = $tmp;
+    }
+    return $arr;
+  }
+}
---
+++ tests/ArrayUtilsTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Schwaen\Stdlib;
+
+/**
+ * ArrayUtilsTest
+ */
+class ArrayUtilsTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Test for Schwaen\Stdlib\ArrayUtils::column
+   */
+  public function testColumn() {
+    $array = [
+      ['id'=>1, 'name'=>'Sven'],
+      ['id'=>2, 'name'=>'Hans'],
+      ['id'=>5, 'name'=>'Peter'],
+    ];
+    $this->assertEquals(['Sven','Hans','Peter'], ArrayUtils::column($array, 'name'));
+    $this->assertEquals([1,2,5], ArrayUtils::column($array, 'id'));
+    $this->assertEquals([1=>'Sven',2=>'Hans',5=>'Peter'], ArrayUtils::column($array, 'name', 'id'));
+  }
+}
