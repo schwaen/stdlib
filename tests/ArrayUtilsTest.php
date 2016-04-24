e723b7ec70902da9de6fe8edebad03f11a37d87c
plajo <s.muhle@plajo.de>
2016-04-24 19:21:43 +0200

    test phpunit on travis ci

--- tests/ArrayUtilsTest.php
+++ tests/ArrayUtilsTest.php
@@ -17,6 +17,6 @@ class ArrayUtilsTest extends \PHPUnit_Framework_TestCase {
     ];
     $this->assertEquals(['Sven','Hans','Peter'], ArrayUtils::column($array, 'name'));
     $this->assertEquals([1,2,5], ArrayUtils::column($array, 'id'));
-    $this->assertEquals([1=>'Sven',2=>'Hans',5=>'Peter'], ArrayUtils::column($array, 'name', 'id'));
+    $this->assertEquals([1=>'Sven',2=>'Hans',5=>'Peterr'], ArrayUtils::column($array, 'name', 'id'));
   }
 }
