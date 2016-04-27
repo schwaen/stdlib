79c8e0ad4e5326c49482289f2f821c1f7f8310a9
plajo <s.muhle@plajo.de>
2016-04-27 18:25:07 +0200

    CS Fix for psr2

--- src/ArrayUtils.php
+++ src/ArrayUtils.php
@@ -4,105 +4,110 @@ namespace Schwaen\Stdlib;
 /**
  * ArrayUtils
  */
-class ArrayUtils {
+class ArrayUtils
+{
+    /**
+     * Return the values from a single column in the input array
+     *
+     * @param array $input
+     * @param string|int $column_key
+     * @param string $index_key
+     * @return null|array
+     */
+    public static function column(array $input, $column_key, $index_key = null)
+    {
+        $arr = array_map(function ($d) use ($column_key, $index_key) {
+        if (!isset($d[$column_key])) {
+            return null;
+        }
+        if ($index_key !== null) {
+            return [$d[$index_key] => $d[$column_key]];
+        }
+        return $d[$column_key];
+      }, $input);
+        if ($index_key !== null) {
+            $tmp = [];
+            foreach ($arr as $ar) {
+                $tmp[key($ar)] = current($ar);
+            }
+            $arr = $tmp;
+        }
+        return $arr;
+    }
 
-  /**
-   * Return the values from a single column in the input array
-   *
-   * @param array $input
-   * @param string|int $column_key
-   * @param string $index_key
-   * @return null|array
-   */
-  public static function column(array $input, $column_key, $index_key = null) {
-    $arr = array_map(function($d) use ($column_key, $index_key) {
-      if(!isset($d[$column_key])) {
-        return null;
-      }
-      if($index_key !== null) {
-        return [$d[$index_key] => $d[$column_key]];
-      }
-      return $d[$column_key];
-    }, $input);
-    if($index_key !== null) {
-      $tmp = [];
-      foreach($arr as $ar) {
-        $tmp[key($ar)] = current($ar);
-      }
-      $arr = $tmp;
+    /**
+     * Test whether an array contains one or more string keys
+     *
+     * @param array $input
+     * @param bool $only
+     * @param bool $allow_empty Should an empty array() return true
+     * @return bool
+     */
+    public static function hasStringKeys(array $input, $only = false, $allow_empty = false)
+    {
+        if (!is_array($input)) {
+            return false;
+        }
+        if (!$input) {
+            return $allow_empty;
+        }
+        return count(array_filter(array_keys($input), 'is_string')) > ($only ? count($input) : 0);
     }
-    return $arr;
-  }
 
-  /**
-   * Test whether an array contains one or more string keys
-   *
-   * @param array $input
-   * @param bool $only
-   * @param bool $allow_empty Should an empty array() return true
-   * @return bool
-   */
-  public static function hasStringKeys(array $input, $only = false, $allow_empty = false) {
-    if(!is_array($input)) {
-      return false;
+    /**
+     * Test whether an array contains one or more integer keys
+     *
+     * @param array $input
+     * @param bool $only
+     * @param bool $allow_empty    Should an empty array() return true
+     * @return bool
+     */
+    public static function hasIntegerKeys(array $input, $only = false, $allow_empty = false)
+    {
+        if (!is_array($input)) {
+            return false;
+        }
+        if (!$input) {
+            return $allow_empty;
+        }
+        return count(array_filter(array_keys($input), 'is_int')) > ($only ? count($input) : 0);
     }
-    if(!$input) {
-      return $allow_empty;
-    }
-    return count(array_filter(array_keys($input), 'is_string')) > ($only ? count($input) : 0);
-  }
 
-  /**
-   * Test whether an array contains one or more integer keys
-   *
-   * @param array $input
-   * @param bool $only
-   * @param bool $allow_empty    Should an empty array() return true
-   * @return bool
-   */
-  public static function hasIntegerKeys(array $input, $only = false, $allow_empty = false) {
-    if(!is_array($input)) {
-      return false;
+    /**
+     * Returns the value of the first found key-name
+     *
+     * @param array $input
+     * @param array $keys
+     * @param string $default
+     * @return mixed
+     */
+    public static function findValueByKeys(array $input, array $keys, $default = null)
+    {
+        $return = $default;
+        foreach ($keys as $key) {
+            if (isset($input[$key])) {
+                $return = $input[$key];
+                break;
+            }
+        }
+        return $return;
     }
-    if(!$input) {
-      return $allow_empty;
-    }
-    return count(array_filter(array_keys($input), 'is_int')) > ($only ? count($input) : 0);
-  }
 
-  /**
-   * Returns the value of the first found key-name
-   *
-   * @param array $input
-   * @param array $keys
-   * @param string $default
-   * @return mixed
-   */
-  public static function findValueByKeys(array $input, array $keys, $default = null) {
-    $return = $default;
-    foreach($keys as $key) {
-      if(isset($input[$key])) {
-        $return = $input[$key];
-        break;
-      }
+    /**
+     * Flatten a multi-dimensional aray into a one dimensional array
+     *
+     * @param array $input
+     * @param string $preserve_keys
+     * @return array
+     */
+    public static function flatten(array $input, $preserve_keys = false)
+    {
+        $return = [];
+        $awr_func = $preserve_keys
+        ? function ($v, $k) use (&$return) {$return[$k] = $v;}
+        : function ($v) use (&$return) {$return[] = $v;}
+      ;
+        array_walk_recursive($input, $awr_func);
+        return $return;
     }
-    return $return;
-  }
-
-  /**
-   * Flatten a multi-dimensional aray into a one dimensional array
-   *
-   * @param array $input
-   * @param string $preserve_keys
-   * @return array
-   */
-  public static function flatten(array $input, $preserve_keys = false) {
-    $return = [];
-    $awr_func = $preserve_keys
-      ? function($v, $k) use(&$return) {$return[$k] = $v;}
-      : function($v) use(&$return) {$return[] = $v;}
-    ;
-    array_walk_recursive($input, $awr_func);
-    return $return;
-  }
 }
--- src/SingletonTrait.php
+++ src/SingletonTrait.php
@@ -4,33 +4,41 @@ namespace Schwaen\Stdlib;
 /**
  * SingletonTrait
  */
-trait SingletonTrait {
+trait SingletonTrait
+{
+    /**
+     * Instance of itselfs
+     * @var self
+     */
+    private static $instance = null;
 
-  /**
-   * Instance of itselfs
-   * @var self
-   */
-  static private $instance = null;
-
-  /**
-   * Return the instance of the used class
-   * @return \Schwaen\Stdlib\self
-   */
-  public static function getInstance() {
-    if(self::$instance === null) {
-      self::$instance = new self();
+    /**
+     * Return the instance of the used class
+     * @return \Schwaen\Stdlib\self
+     */
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
     }
-    return self::$instance;
-  }
 
-  /**
-   * private constructor for calling an optional init-Method
-   */
-  final private function __construct() {
-    $this->init();
-  }
+    /**
+     * private constructor for calling an optional init-Method
+     */
+    final private function __construct()
+    {
+        $this->init();
+    }
 
-  private function init() {}
-  final private function __clone() {}
-  final private function __wakeup() {}
+      private function init()
+      {
+      }
+      final private function __clone()
+      {
+      }
+      final private function __wakeup()
+      {
+      }
 }
