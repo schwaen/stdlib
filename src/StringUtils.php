e4594eb5c5e2cb050f7b5a526087b10876213a72
schwaen <s.muhle@plajo.de>
2016-04-27 15:47:12 +0200

    cs fix #3

--- src/StringUtils.php
+++ src/StringUtils.php
@@ -6,36 +6,35 @@ namespace Schwaen\Stdlib;
  */
 class StringUtils
 {
-
-  /**
-   * Split a string on every UpperCase letter
-   * @access public
-   * @static
-   * @param string $string
-   * @throws \InvalidArgumentException
-   * @return array
-   */
-  public static function splitOnUpperCase($string)
-  {
-    if (!is_string($string)) {
-      throw new \InvalidArgumentException('$string should be a string. '.gettype($string).' is given');
+    /**
+     * Split a string on every UpperCase letter
+     * @access public
+     * @static
+     * @param string $string
+     * @throws \InvalidArgumentException
+     * @return array
+     */
+    public static function splitOnUpperCase($string)
+    {
+        if (!is_string($string)) {
+            throw new \InvalidArgumentException('$string should be a string. '.gettype($string).' is given');
+        }
+        return preg_split('/(?=[A-Z])/', $string, -1, PREG_SPLIT_NO_EMPTY);
     }
-    return preg_split('/(?=[A-Z])/', $string, -1, PREG_SPLIT_NO_EMPTY);
-  }
-
-  /**
-   * Split a string on every LowerCase letter
-   * @access public
-   * @static
-   * @param string $string
-   * @throws \InvalidArgumentException
-   * @return array
-   */
-  public static function splitOnLowerCase($string)
-  {
-    if (!is_string($string)) {
-      throw new \InvalidArgumentException('$string should be a string. '.gettype($string).' is given');
+  
+    /**
+     * Split a string on every LowerCase letter
+     * @access public
+     * @static
+     * @param string $string
+     * @throws \InvalidArgumentException
+     * @return array
+     */
+    public static function splitOnLowerCase($string)
+    {
+        if (!is_string($string)) {
+            throw new \InvalidArgumentException('$string should be a string. '.gettype($string).' is given');
+        }
+        return preg_split('/(?=[a-z])/', $string, -1, PREG_SPLIT_NO_EMPTY);
     }
-    return preg_split('/(?=[a-z])/', $string, -1, PREG_SPLIT_NO_EMPTY);
-  }
 }
