fc639f7ddc9139ad95f7fd073bed5227983b52f8
schwaen <s.muhle@plajo.de>
2016-04-27 15:53:21 +0200

    CS fix #5

--- src/DateTimeUtils.php
+++ src/DateTimeUtils.php
@@ -6,45 +6,44 @@ namespace Schwaen\Stdlib;
  */
 class DateTimeUtils
 {
+    /**
+     * seconds per minute
+     * @var int
+     */
+    const SECONDS_PER_MINUTE = 60;
 
-  /**
-   * seconds per minute
-   * @var int
-   */
-  const SECONDS_PER_MINUTE = 60;
+    /**
+     * seconds per hour
+     * 60 * 60
+     * @var int
+     */
+    const SECONDS_PER_HOUR = 3600;
 
-  /**
-   * seconds per hour
-   * 60 * 60
-   * @var int
-   */
-  const SECONDS_PER_HOUR = 3600;
+    /**
+     * seconds per day
+     * 60 * 60 * 24
+     * @var int
+     */
+    const SECONDS_PER_DAY = 86400;
 
-  /**
-   * seconds per day
-   * 60 * 60 * 24
-   * @var int
-   */
-  const SECONDS_PER_DAY = 86400;
+    /**
+     * seconds per week
+     * 60 * 60 * 24 * 7
+     * @var int
+     */
+    const SECONDS_PER_WEEK = 604800;
 
-  /**
-   * seconds per week
-   * 60 * 60 * 24 * 7
-   * @var int
-   */
-  const SECONDS_PER_WEEK = 604800;
+    /**
+     * seconds per month
+     * 60 * 60 * 24 * 30
+     * @var int
+     */
+    const SECONDS_PER_MONTH = 2592000;
 
-  /**
-   * seconds per month
-   * 60 * 60 * 24 * 30
-   * @var int
-   */
-  const SECONDS_PER_MONTH = 2592000;
-
-  /**
-   * seconds per year
-   * 60 * 60 * 24 * 365
-   * @var int
-   */
-  const SECONDS_PER_YEAR = 31536000;
+    /**
+     * seconds per year
+     * 60 * 60 * 24 * 365
+     * @var int
+     */
+    const SECONDS_PER_YEAR = 31536000;
 }
